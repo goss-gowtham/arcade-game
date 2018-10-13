1c8bb139abbbeaf4cfdc0f1ffea5644638f69058
Goss <gossgowtham@gmail.com>
2018-10-13 20:59:04 +0530

    Updated player and bug. Bug is moving random speed and position

--- js/app.js
+++ js/app.js
@@ -1,36 +1,79 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
+    constructor(x,y,speed){
+        this.x = x;
+        this.y = y + 60;
+        this.blockWidth = 101;
+        this.sprite = 'images/enemy-bug.png';
+        this.speed = speed;
+    }
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+    update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+        if(this.x < this.blockWidth * 5){
+          this.x += dt * this.speed;
+        }
+        else{
+          this.x = 0;
+        }
+    }
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+    render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class Hero{
+  constructor(){
+    this.blockWidth = 101;    //got from for loop of drawImage()
+    this.blockHeight = 83;    //blockWidth - left/right, blockHeight - up/down moves
+    this.sprite = 'images/char-boy.png';
+    this.startX = this.blockWidth * 2;
+    this.startY = (this.blockHeight *4)+60;
+    this.x = this.startX;
+    this.y = this.startY;
+  }
+  render(){
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+  }
+  handleInput(input){
+    if(input == 'left' && this.x > 0){
+        this.x -= this.blockWidth;
+    }
+    else if(input == 'right' && this.x < this.blockWidth*4){
+        this.x += this.blockWidth;
+    }
+    else if(input == 'up' && this.y > 0){
+        this.y -= this.blockHeight;
+    }
+    else if(input == 'down' && this.y < this.blockHeight*4){
+        this.y += this.blockHeight;
+    }
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const player = new Hero();
+const bug = new Enemy(-101,0,250);
+const bug1 = new Enemy(81,83,250);
+const bug2 = new Enemy(201,83,250);
+const bug3 = new Enemy(201,163,300);
+const allEnemies = [];
+allEnemies.push(bug,bug1,bug2,bug3);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
